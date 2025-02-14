5fcf2595827525dcb8da7c67bfa5f16e814b3698
nico <nico.chan@yahoo.com>
2025-02-14 17:23:30 +0800

    2nd

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,24 @@
 import React from "react";
+import HeaderBox from "@/components/HeaderBox";
+
 
 const Home = () => {
+    const LoggedIn = { firstName: 'Nico'};
+
   return (
     <div>
-        Home    
+        <section className="home">
+            <div className="home-content">
+                <div className="home-header">
+                    <HeaderBox 
+                        type="greeting"
+                        title="Welcome"
+                        user={LoggedIn.firstName || 'Guest'}
+                        subtext="Access and manage your account and transactions efficiently."
+                    />
+                </div>
+            </div>
+        </section>  
     </div>
   )
 }
---
+++ components/HeaderBox.tsx
@@ -0,0 +1,19 @@
+const HeaderBox = ({type = "title", title, user, subtext}: HeaderBoxProps) => {
+  return (
+    <div className="header-box">
+        <h1 className="header-box-title">
+                {title}
+                {type === "greeting" && (
+                    <span className="text-bankGradient">
+                        &nbsp;{user}
+                    </span>
+                )}
+        </h1>
+        <h1>
+            <p className="header-box-subtext">{subtext}</p>
+        </h1>
+    </div>
+  )
+}
+
+export default HeaderBox
